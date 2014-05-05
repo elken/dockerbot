e05db303e3ede38a14fbdbb8f13582d49cae4559
Ellis kenyo <elken@users.noreply.github.com>
2014-05-05 17:18:50 +0100

    Commented code, cleaned up a little.

--- dockerbot.rb
+++ dockerbot.rb
@@ -3,23 +3,33 @@ require 'json'
 require 'mail'
 require 'date'
 
+# Please change everything in < > and don't complain to me about those errors.
+
+# Options hash table for Mail
 options = { 
-		:address	      => "smtp.gmail.com",
-		:port		      => 587,
-		:domain		      => 'gmail.com',
-		:user_name	      => '<username>',
-		:password	      => '<password>',
-		:authentication       => 'plain',
-		:enable_starttls_auto => true
+		:address	      => "<smtp address>",   # This can be localhost if you use a local SMTP server
+		:port		      => <port_number>,      # Again, this can be whatever you use
+		:domain		      => '<address_domain>', # Domain of your email "gmail.com", "hotmail.co.uk", etc
+		:user_name	      => '<username>', 	     # Whatever you use to login to access your mailbox
+		:password	      => '<password>',       # The password to said mailbox
+		:authentication       => 'plain',            # How your SMTP server authenticicates
+		:enable_starttls_auto => true                # Use StartTLS automagically
 }
 
+# Assign the options to the object
 Mail.defaults do
   delivery_method :smtp, options
 end
 
-post '/payload' do
+# Use sinatra to listen for a request
+post '<url>' do
   push = JSON.parse(request.body.read)
-  puts "I got some JSON: #{push.inspect}"
+  
+  # Optional line for debugging the request
+  # An example reference request is located on the webhooks page
+  # puts "#{push.inspect}"
+  
+  # Compose and send email
   Mail.deliver do
     to '<recipient>'
     from '<sender>'
